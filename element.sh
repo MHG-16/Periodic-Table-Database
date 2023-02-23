d6f11fe1e678cd2d23061b5bcaeedf4e2f8234d4
MHG-16 <hammahadi2006@gmail.com>
2023-02-23 14:05:26 +0000

    fix: empty input

--- element.sh
+++ element.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --non-align --tuples-only -C"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument"
+else
+  if [[ ! $1 =~ ^[0-9]+$ ]]
+  then
+    echo "$1 is not number"
+  fi
+  else
+
+fi
